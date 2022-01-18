8713cabb5b19f791b2437e1842717d4dc438f559
razetime <raghuallthetime@hotmail.com>
2022-01-18 14:46:25 +0530

    move to random-rec

--- miscs/tests/asciiresize.cpp
+++
@@ -1,3 +0,0 @@
-#include "/Users/ranga/stdc++.h"
-namespace std{void f(){string a,b;for(int i:{1,3}){getline(cin,a);b.resize(max(a.size()/2,b.size()));for(int j{};j<a.size();j++)b[j/2]+=(a[j]>32)*(1<<(i-j%2));}for(int i:b)cout.write("⠀▝▘▀▗▐▚▜▖▞▌▛▄▟▙█"+i*3,3);cout<<endl;if(cin)f();}}
-int main(){std::f();}
--- miscs/tests/tableExtract.py
+++
@@ -1,4 +0,0 @@
-from tabula import read_pdf
-#Uses stabula to read tables from a pdf
-df=read_pdf("<Insert pdf name here>", output_format="json", pages="all",output_path="WHO-data.json")
-df
